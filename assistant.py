e21a9470787da68b456777bcc012996c0b902339
ejakait <ejakaitcaleb@outlook.com>
2016-12-03 23:52:48 +0300

    dot env setup with python-dotenv

---
+++ assistant.py
@@ -0,0 +1,19 @@
+#assistant.py
+
+import telepot, time 
+from settings import *
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print ('Got command: %s' % command)
+
+    if command == '/hello':
+        bot.sendMessage(chat_id, "Hello, how are you?")
+
+# Create a bot object with API key
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+# Listen to the messages
+while 1:
+    time.sleep(10)
